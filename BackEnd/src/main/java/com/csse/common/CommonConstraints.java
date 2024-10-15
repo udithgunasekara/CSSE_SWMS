f8933ed38d2dfe0deafda5cd3c383bb4f9809098
Dilshan Gamage <dilshangamage107@gmail.com>
2024-10-15 13:14:35 +0530

    created dto for trashbin

--- BackEnd/src/main/java/com/csse/common/CommonConstraints.java
+++ BackEnd/src/main/java/com/csse/common/CommonConstraints.java
@@ -4,4 +4,5 @@ public class CommonConstraints {
     public static final String COL_MODEL_COLLECTION_NAME = "collection_models";
     public static final String CITY_COLLECTION_NAME = "city";
     public static final String P_FACILITY_COLLECTION_NAME = "processing_facility";
+    public static final String TRASHBIN_COLLECTION_NAME = "trashbin";
 }
